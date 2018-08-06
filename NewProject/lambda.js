7555787cc891ebfc69d75d9df4e6d87d560f1b81
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2018-08-06 13:49:01 +0530

    commit 3

--- NewProject/lambda.js
+++ NewProject/lambda.js
@@ -1,6 +1,68 @@
 let AWS = require('aws-sdk');
+let google = require('googleapis').google;
+let _auth = require('./Authorizer');
+const datastore = google.datastore('v1');
 
-exports.handler = function(event, context, callback) {
-    
-    callback(null,'Successfully executed');
+exports.handler = function (event, context, callback) {
+    datastore.projects.runQuery({
+        projectId: process.env.GCLOUD_PROJECT_ID,
+        resource: {
+            gqlQuery: {
+                queryString: "SELECT*FROM Test;",
+                allowLiterals: true,
+                namedBindings: {
+
+                },
+                positionalBindings: [
+
+                ]
+            }
+        }
+    }).then(response => {
+        console.log(response.data);           // successful response
+        /*
+        response.data = {
+            "batch": {
+                "entityResultType": "FULL",
+                "endCursor": "<base64-encoded>",
+                "entityResults": [
+                    {
+                        "entity": {
+                            "key": {
+                                "partitionId": {
+                                    "projectId": "<project>"
+                                },
+                                "path": [{
+                                        "kind": "<kind>",
+                                        "name": "<name>"
+                                    }
+                                ]
+                            },
+                            "properties": {
+                                "<key-1>": {
+                                    "<type-1>": "<value-1>"
+                                },
+                                ...
+                            }
+                        },
+                        "cursor": "<cursor>",
+                        "version": "<version-timestamp>"
+                    },
+                    ...
+                ]
+            },
+            "query": {
+                "kind": <kind-spec>,
+                "filter": {
+                    "propertyFilter": <property-spec; property, op and value>
+                }
+            }
+        }
+        */
+    })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
+    callback(null, 'Successfully executed');
 }
